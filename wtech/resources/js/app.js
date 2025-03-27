658c22df281f237ce5057fa2de5e760a2ebce5c5
ByMikiii <mikixr1@gmail.com>
2025-03-27 14:03:10 +0100

    Add Filter

--- wtech/resources/js/app.js
+++ wtech/resources/js/app.js
@@ -1,9 +1,29 @@
+// toggle mobile navbar with button
 function toggleMobileNav() {
   const navElement = document.getElementById("mobile-nav");
-  if (navElement.style.display === "none") {
-    navElement.style.display = "block";
-  } else {
+  if (navElement.style.display === "block") {
     navElement.style.display = "none";
+  } else {
+    navElement.style.display = "block";
   }
-  
 }
+
+// size select buttons
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".size-select");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("selected");
+    });
+  });
+});
+
+// color select buttons
+document.addEventListener("DOMContentLoaded", function () {
+  const colorButtons = document.querySelectorAll(".color-select");
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("selected");
+    });
+  });
+});

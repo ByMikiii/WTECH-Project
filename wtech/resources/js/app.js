007b86e1b951c19905cdcb12fa0bfac5d09ea8da
ByMikiii <mikixr1@gmail.com>
2025-03-29 21:26:06 +0100

    Add responsive filter

--- wtech/resources/js/app.js
+++ wtech/resources/js/app.js
@@ -8,6 +8,29 @@ function toggleMobileNav() {
   }
 }
 
+// toggle mobile filter with button
+function toggleMobileFilter() {
+  const filterElement = document.getElementById("mobile-filter");
+  console.log(filterElement.style.display)
+  if (filterElement.style.display === "none") {
+    filterElement.style.display = "flex";
+  } else {
+    filterElement.style.display = "none";
+  }
+  console.log(filterElement.style.display)
+}
+
+// automatically hide nav & filter when resizing
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 770) {
+    document.getElementById("mobile-nav").style.display = "none";
+  }
+  if (window.innerWidth > 600) {
+    document.getElementById("mobile-filter").style.display = "none";
+  }
+});
+
+
 // size select buttons
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".size-select");
